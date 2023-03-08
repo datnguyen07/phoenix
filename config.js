834eea7c4c19db609e50c2db3fcd7bdb23483553
datnguyen07 <73738282+datnguyen07@users.noreply.github.com>
2023-03-08 23:47:01 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Phượng';
+const giftUrl = 'https://datnguyen07.github.io/tim/';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng Phượng';
+const contentCard = 'Chúc Phượng 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Phượng trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
